5e07a7144b074596d7d00c1da2ae136eeb120e27
naveenkumarrb <101310360+NaveenKumarRB@users.noreply.github.com>
2025-09-08 12:57:22 +0530

    Create Script to Install Apache in ubuntu.sh

---
+++ Script to Install Apache in ubuntu.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo apt update                    #To update the system.
+sudo apt install apache2           # To install apache in system
+sudo systemctl status apache2      # To check the status of apache
+sudo systemctl enable apache2      # To enable the apache     
+#Adjust the firewall (if UFW is active): If you are using UFW (Uncomplicated Firewall), you need to allow HTTP traffic. Apache registers profiles with UFW for this purpose#
+#You will see profiles like Apache, Apache Full, and Apache Secure. Apache: Opens port 80 (HTTP). Apache Full: Opens ports 80 (HTTP) and 443 (HTTPS). Apache Secure: Opens port 443 (HTTPS) only#
+sudo ufw app list                  # List all Firewall status of the system.
+sudo ufw allow 'Apache'            # Allow the Apache Firewall incoming traffic
