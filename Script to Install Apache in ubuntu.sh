7175b02ca835626f49359b8de22a630044cd2cf5
naveenkumarrb <101310360+NaveenKumarRB@users.noreply.github.com>
2025-09-08 14:57:26 +0530

    Update and rename Script to Install Apache in ubuntu.sh to None

--- Script to Install Apache in ubuntu.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "Installing Apache on Ubuntu..."
-sudo apt update -y
-sudo apt install -y apache2
-sudo systemctl start apache2
-sudo systemctl enable apache2
-echo "<h1>Welcome to Rabinado - Ubuntu</h1>" | sudo tee /var/www/html/index.html
