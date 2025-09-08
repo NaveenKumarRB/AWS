f0a19c5449f32a6394348ff453c6c9aa6e38d101
naveenkumarrb <101310360+NaveenKumarRB@users.noreply.github.com>
2025-09-08 13:59:54 +0530

    Update Script to Install Apache in ubuntu.sh

--- Script to Install Apache in ubuntu.sh
+++ Script to Install Apache in ubuntu.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
-sudo apt update                    #To update the system.
-sudo apt install apache2 -y          #To install apache in system
-sudo systemctl status apache2      #To check the status of apache
-sudo systemctl enable apache2      #To enable the apache     
-
-sudo nano /var/www/html/index.html  #to create the index file
-echo "Welcome to my ec2 learnings" >/var/www/html/index.html #add data to index.html
-sudo systemctl restart apache2       #To restart the services
-
-#Adjust the firewall (if UFW is active): If you are using UFW (Uncomplicated Firewall), you need to allow HTTP traffic. Apache registers profiles with UFW for this purpose#
-#You will see profiles like Apache, Apache Full, and Apache Secure. Apache: Opens port 80 (HTTP). Apache Full: Opens ports 80 (HTTP) and 443 (HTTPS). Apache Secure: Opens port 443 (HTTPS) only#
-
-#sudo ufw app list                  #List all Firewall status of the system.
-#sudo ufw allow 'Apache'            #Allow the Apache Firewall incoming traffic
+echo "Installing Apache on Ubuntu..."
+sudo apt update -y
+sudo apt install -y apache2
+sudo systemctl start apache2
+sudo systemctl enable apache2
+echo "<h1>Welcome to Rabinado - Ubuntu</h1>" | sudo tee /var/www/html/index.html
